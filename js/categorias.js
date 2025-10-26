6b22f5a9db5042cce338b92d602548fb988b8dfa
jorgearauz115-oss <jorgearauz115@gmail.com>
2025-10-25 23:50:46 -0400

    Update categorias.js

--- js/categorias.js
+++ js/categorias.js
@@ -1,1 +1,16 @@
+// Lista automática de categorías
+const categorias = [
+    { nombre: "Periféricos", link: "perifericos/index.html" },
+    { nombre: "Procesadores", link: "procesadores/index.html" }
+];
+
+// Insertar automáticamente en el HTML
+const container = document.querySelector(".categorias");
+
+categorias.forEach(cat => {
+    const div = document.createElement("div");
+    div.classList.add("categoria-card");
+    div.innerHTML = `<a href="${cat.link}">${cat.nombre}</a>`;
+    container.appendChild(div);
+});
 
